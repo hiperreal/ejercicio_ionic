d313d755e7951f6e03001fcb555bff87ed09d1b6
hiperreal <abihudcastrillo@gmail.com>
2026-04-27 16:17:03 -0600

    cambios nuevos

--- MIprimeraApp/src/app/services/task.service.ts
+++ MIprimeraApp/src/app/services/task.service.ts
@@ -19,7 +19,7 @@ export class TaskService{
         this.tasks =value ? JSON.parse(value) : [];
     }
     /* Devuelve la lista de tareas */
-    getTaks(): Task[] {
+    getTasks(): Task[] {
         return [...this.tasks];
     }
 
--- MIprimeraApp/src/app/tab1/tab1.page.ts
+++ MIprimeraApp/src/app/tab1/tab1.page.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import {
+ IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonList,
+  IonItem, 
+  IonLabel,
+  IonButton,
+  IonButtons,
+  IonIcon,
+  IonBadge,
+  IonSelect,
+  IonSelectOption } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../services/task.service';
+import { Task, Taskstatus } from '../models/task.model';
+import { trash, create, checkmarkDone } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+     IonHeader,
+     IonToolbar, 
+     IonTitle, 
+     IonContent,
+     CommonModule,
+     IonList,
+     IonItem,
+     IonLabel,
+     IonButton,
+     IonButtons,
+     IonIcon,
+     IonSelect,
+     IonSelectOption
+    ],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit{
+  task: Task[] =[]
+  constructor(private TaskService: TaskService) {
+    addIcons({trash, create, checkmarkDone})
+  }
+  ngOnInit(): void {
+    this.cargarTareas();
+  }
+  ionViewWillEnter(): void{
+    this.cargarTareas();
+  }
+  cargarTareas(): void {
+    this.task =this.TaskService.getTasks();
+  }
+  async eliminar(id: string): Promise<void>{
+    await this.TaskService.deleteTask(id);
+    this.cargarTareas();
+  }
+  async cambiarEstado(id: string,estado:Taskstatus): Promise<void>{
+    await this.TaskService.changeStatus(id, estado);
+    this.cargarTareas();    
+  }
+
 }
